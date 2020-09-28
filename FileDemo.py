f7a2c788ae7b37b127e3f34a2c26a90c97024239
mdelafuen <mdelafuentemartinez@student.bridgew.edu>
2020-09-28 13:32:30 -0400

    insert

--- FileDemo.py
+++ FileDemo.py
@@ -1,6 +1,14 @@
 
 def main():
     file = open("Silly.txt", 'r')
+    all_of_file = file.readlines()
+    print(all_of_file)
+
+def reverser():
+    rev_file = open("Reversable.txt", 'r')
+    all_lines = rev_file.readlines()
+    to_insert = all_lines[0]
+    print(f"welcome to {to_insert}, Fantastic Student")
 
 
-main()
+reverser()
