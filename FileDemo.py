70f31df56e82823d7f6dd9d84b9dd02a5ef3786f
mdelafuen <mdelafuentemartinez@student.bridgew.edu>
2020-09-28 15:22:18 -0400

    practice for quiz 1

--- FileDemo.py
+++ FileDemo.py
@@ -10,5 +10,14 @@ def reverser():
     to_insert = all_lines[0]
     print(f"welcome to {to_insert}, Fantastic Student")
 
+def slice():
+    question1a = "go to the store"
+    question1b = "Sufferin' Succotash!"
+    answer1b = question1b.replace("S", "Th")
+    print(answer1b)
+    answer1a = question1a[:10] + question1a[11:]
+    print(answer1a)
 
-reverser()
+# main()
+#reverser()
+slice()
